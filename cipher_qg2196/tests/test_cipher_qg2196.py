6a97e158822c8b3124b7b30df2347395e2bc11e6
Qianrun Guo <qg2196@columbia.edu>
2020-11-16 00:32:17 -0800

    HW7 Question 3

--- cipher_qg2196/tests/test_cipher_qg2196.py
+++ cipher_qg2196/tests/test_cipher_qg2196.py
@@ -2,7 +2,7 @@ from cipher_qg2196 import __version__
 from cipher_qg2196.cipher_qg2196 import cipher
 
 def test_version():
-    assert __version__ == '0.1.0'
+    assert __version__ == '0.2.0'
 
 import pytest
 
