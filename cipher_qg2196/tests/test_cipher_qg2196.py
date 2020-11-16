3ffb91d1d379678fa98906823e628770ee24b351
Qianrun Guo <qg2196@columbia.edu>
2020-11-16 00:11:27 -0800

    HW 7 Question 3

--- cipher_qg2196/tests/test_cipher_qg2196.py
+++ cipher_qg2196/tests/test_cipher_qg2196.py
@@ -1,5 +1,28 @@
 from cipher_qg2196 import __version__
-from cipher_qg2196 import cipher_qg2196
+from cipher_qg2196.cipher_qg2196 import cipher
 
 def test_version():
     assert __version__ == '0.1.0'
+
+import pytest
+
+def test_1a():
+    text = 'guys'
+    shift = 1
+    expected = 'hvzt'
+    actual = cipher(text, shift)
+    assert actual == expected
+    
+def test_1b():
+    text = 'guys'
+    shift = -1
+    expected = 'ftxr'
+    actual = cipher(text,shift)
+    assert actual == expected
+    
+def test_1c():
+    text = 'guys!'
+    shift = 1
+    expected = 'hvzt!'
+    actual = cipher(text, shift)
+    assert actual == expected
